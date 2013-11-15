196765b997bb8612e9972bc1e4adf9235929d6fc
brandonfwl <brandonfwl@gmail.com>
2013-11-15 12:59:38 -0800

    Changed LiveServerTestCase default socket, now passing functional test cases

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,8 +1,10 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import unittest
+import os
+os.environ['DJANGO_LIVE_TEST_SERVER_ADDRESS'] = 'localhost:8082'
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -19,7 +21,7 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         #Edith has heard about a cool new online to-do app.  She goes
         # to check out it's home_page
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
 
         #she notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
@@ -55,6 +57,3 @@ class NewVisitorTest(unittest.TestCase):
         # The page updates again, and now shows both items on her list
         self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
         self.check_for_row_in_list_table('1: Buy peacock feathers')
-
-if __name__ == '__main__':
-    unittest.main(warnings='ignore')
